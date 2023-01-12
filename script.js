a50ae6f6125bdc2b2e6bd92c8d97ac1af472071b
David Godard <davidgodard76700@gmail.com>
2023-01-12 11:54:40 +0100

    maj

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-function previewFile() {
-  const preview = document.querySelector('img');
-  const file = document.querySelector('input[type=file]').files[0];
-  const reader = new FileReader();
 
-  reader.addEventListener("load", () => {
-    // convert image file to base64 string
-    preview.src = reader.result;
-  }, false);
-
-  if (file) {
-    reader.readAsDataURL(file);
-  }
-}
 function addInput() {
   let input = document.querySelectorAll('input[type=file]');
   console.log(input);
@@ -20,10 +7,12 @@ function addInput() {
   if (input.length <= 2) {
 
 
-    let newInput = document.createElement('input');
-    newInput.type = 'file';
-    newInput.id = ` image ${i}`;
-    document.querySelector('.container').appendChild(newInput);
+    let newDiv = document.createElement('div');
+
+    newDiv.innerHTML = `<input type="file" name="userFile${i}" oninput="pic${i}.src=window.URL.createObjectURL(this.files[0])" />
+    <span><?= $messages['userFile'${i}] ?? '' ?></span>
+     <br><img id =pic${i} src="" height="200" alt="Image preview" /> <br>`
+    document.querySelector('form').appendChild(newDiv);
   }
 
 };
